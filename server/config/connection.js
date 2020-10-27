5af1bb9d09a093885d9f43b0e7b54e1f65f16b2e
Jake Kelly <jakelly@honorhealth.com>
2020-10-27 11:37:58 -0700

    v1

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/remotesocial', 
+  process.env.MONGODB_URI || 'mongodb://localhost/movielist', 
     {
     useNewUrlParser: true,
     useUnifiedTopology: true,
